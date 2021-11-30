8d4cccdd52a4a5cea2abc43a00b0d0d36f747023
Gepsu <joni.nyqvist@netikka.fi>
2021-11-30 19:55:30 +0200

    Added argument support
    Int, float, string and boolean

--- addons/console/Console.gd
+++ addons/console/Console.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 class_name Console
 
 const connections = {}
+const arguments = {}
 
 export var size : float = 200
 
@@ -19,6 +20,8 @@ func _ready() -> void:
 	add_command("clear", funcref(self, "clear"))
 	add_command("commands", funcref(self, "commands"))
 	add_command("help", funcref(self, "help"))
+	add_command("found-a-bug", funcref(self, "bug"))
+	add_command("github", funcref(self, "github"))
 	output("Welcome to [color=#FFA500]Geppy's Console v0.1[/color]", false)
 	output("Type '[color=yellow]help[/color]' if you'd like more information about the addon", false)
 
@@ -45,6 +48,7 @@ func open() -> void:
 	tween.stop_all()
 	tween.interpolate_property(control, "rect_size:y", control.rect_size.y, size, .5, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
 	tween.start()
+	input.grab_focus()
 
 func close() -> void:
 	input.hide()
@@ -53,11 +57,58 @@ func close() -> void:
 	tween.start()
 	input.release_focus()
 
-func command(cmd : String) -> void:
+func command(msg : String) -> void:
+	# Separate command and arguments
+	var cmd = msg
+	var args : Array = cmd.split(" ")
+	if args.size() > 1:
+		cmd = args.pop_front()
+		# Get rid of excess arguments
+		while args.size() > arguments[cmd].size():
+			args.pop_back()
+		
 	if cmd in connections.keys():
 		var function = connections[cmd] as FuncRef
-		output("[color=yellow]%s[/color]" % cmd)
-		function.call_func()
+		
+		if !arguments[cmd].empty(): # If the command contains arguments
+			# Make sure arguments are correct type
+			var wrong_arg : bool = false
+			for i in args.size():
+				var arg = args[i] as String
+				match arguments[cmd][i]:
+					TYPE_INT:
+						if !arg.is_valid_integer():
+							wrong_arg = true
+							break
+						arg = int(arg)
+					TYPE_REAL:
+						if !arg.is_valid_float():
+							wrong_arg = true
+							break
+						arg = float(arg)
+					TYPE_BOOL:
+						if arg.to_lower() != "true" and arg.to_lower() != "false":
+							wrong_arg = true
+							break
+						arg = arg.to_lower() == "true"
+					TYPE_STRING:
+						pass
+					_: # Not supported type
+						output("[color=red]One or more argument types are not supported ('%s')[/color]" % msg)
+						return
+				
+				# If we made it this far then we got the right type
+				args[i] = arg
+						
+			if wrong_arg:
+				output("[color=red]One or more argument are not the correct type ('%s')[/color]" % msg)
+				return
+			
+			function.call_funcv(args)
+				
+		else: # If it doesn't contain arguments then just run the command
+			output("[color=yellow]%s[/color]" % cmd)
+			function.call_func()
 	else:
 		output("[color=red]Unknown command ('%s')[/color]" % cmd)
 
@@ -70,23 +121,22 @@ func output(msg : String, display_time : bool = true) -> void:
 	
 	output.bbcode_text += "%s%s\n" % [time_string, msg]
 
-func _on_Tween_tween_all_completed() -> void:
-	if open:
-		input.grab_focus()
-	else:
-		input.release_focus()
-
 func _on_TextEdit_text_changed() -> void:
 	if input.get_line_count() > 1:
 		if input.text != "\n":
 			command(input.text.replace("\n", ""))
 		input.text = ""
 
-static func add_command(cmd : String, function : FuncRef) -> void:
-	if connections.has(cmd):
-		print("Console - Command already exists ('%s') !!" % cmd)
+static func add_command(cmd : String, function : FuncRef, args : Array = []) -> void:
+	if " " in cmd:
+		printerr("Console - Spaces are not allowed in commands! ('%s')" % cmd)
+	elif connections.has(cmd):
+		printerr("Console - Command already exists! ('%s')" % cmd)
+	elif !function.is_valid():
+		printerr("Console - Function is not valid! ('%s')" % cmd)
 	else:
 		connections[cmd] = function
+		arguments[cmd] = args
 
 # Built-in Commands
 func clear() -> void:
@@ -97,10 +147,23 @@ func commands() -> void:
 	for cmd in connections.keys():
 		output("- [color=yellow]%s[/color]" % cmd, false)
 
+func bug() -> void:
+	output("[color=#FFA500]Opening issues page in browser...[/color]", false)
+	OS.shell_open("https://github.com/Gepsu/godot-console/issues")
+
+func github() -> void:
+	output("[color=#FFA500]Opening GitHub page in browser...[/color]", false)
+	OS.shell_open("https://github.com/Gepsu/godot-console")
+
 func help() -> void:
 	output("To add commands to this console, you type [color=yellow]Console.add_command(\"command\", " +
 			"funcref(object, \"function\"))[/color] somewhere in your script. Object being the script that " +
 			"contains the function you're trying to add (typically 'self'). Once you've done that, you will " +
-			"be able to type that command in here and run the function while your game is running.", false)
+			"be able to type that command in here and run the function while your game is running. Additionally " +
+			"you may also want to use arguments. This addon supports 4 types ([color=#44a6c6]TYPE_INT, TYPE_REAL," +
+			"TYPE_STRING, TYPE_BOOL[/color]). To add arguments to your command, you add a third argument. " +
+			"An array that contains the types you need in the correct order. [color=yellow]" +
+			"Console.add_command(\"command\", funcref(object, \"function\"), [TYPE_INT, TYPE_BOOL])[/color] " +
+			"for example.", false)
 	output("", false)
 	output("For a list of all the commands, type '[color=yellow]commands[/color]'", false)
